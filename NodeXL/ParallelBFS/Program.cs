528f80e2f44a1549f33201244b02a359e8a7e89b
James Ihrig <JEIhrig@GMail.com>
2014-11-06 21:38:35 -0500

    Assign thread ID to each node in list
    We now assign thread ID to each node in list for the 1D partitioning
    solution.

--- NodeXL/ParallelBFS/Program.cs
+++ NodeXL/ParallelBFS/Program.cs
@@ -72,129 +72,34 @@ namespace ParallelBFS
             }
             //Parallel.ForEach
 
-            for (int i = 0; i < NUM_THREADS; i++)
+            for (int threadNum = 0; threadNum < NUM_THREADS; i++)
             {
                 int remainder = graphSize % NUM_THREADS;
                 List<IVertex> threadVertices = new List<IVertex>();
 
-                int startIndex = i * numSubgraphVertices;
+                int startIndex = threadNum * numSubgraphVertices;
 
                 List<IVertex> graphAsList = g.Vertices.Cast<IVertex>().ToList();
 
-                if (i == NUM_THREADS - 1)
+                if (threadNum == NUM_THREADS - 1)
                 {
                     numSubgraphVertices += remainder;
                 }
 
+                //Assign thread ID to each vertex
+                for (int j = startIndex; j < numSubgraphVertices; j++)
+                {
+                    graphAsList[j].threadID = threadNum;
+                }
                 List<IVertex> subGraph = graphAsList.GetRange(startIndex, numSubgraphVertices);
-                
-                partition.Add(new OneDimensionalPartition(subGraph, i));
-                threadList.Add(new Thread(new ThreadStart(partition[i].bfs)));
+
+                partition.Add(new OneDimensionalPartition(subGraph, threadNum));
+                threadList.Add(new Thread(new ThreadStart(partition[threadNum].bfs)));
                 //BFSOnOneDimensionalPartitioning(subGraph, i);
             }
 
             //Parallel.ForEach<
         }
-        
-        //static void BFSOnOneDimensionalPartitioning(IGraph subGraph)
-        static void BFSOnOneDimensionalPartitioning(List<IVertex> subGraph, int myThreadID )
-        {
-            // L = level of vertices serched
-
-            //Lva = (L sub (v sub s))
-            //                     / 0, v = (v sub s), where (v sub s) is a source
-            //1: Initilize Lva(v) <
-            //                     \ inf, otherwise
-            // 2: for l = 0 to inf do
-            // 3:   F <- {v|Lva(v) = l}, the set of all local vertices with level l
-            // 4:   if F = null for all processors then
-            // 5:       Terminate main loop
-            // 6:   end if
-            // 7:   N <- {neighbors of vertices in f (not neccessarily local
-            // 8:   for all processors q do
-            // 9:       Nq <- { vertices in N owned by processor q}
-            //10:       Send Nq to processor q
-            //11:       Receive N`q from processor q
-            //12:   end for
-            //13:   N` <- Uq N'q ( Ther N`q may overlap)
-            //14:   for v elementOf N` and Lvs(v) = inf do
-            //15:       Lvs(v) <- l + 1
-            //16:   endfor
-            //17: end for
-
-
-            for (UInt32 currentLevel = 0; currentLevel < UInt32.MaxValue; currentLevel++ ) // Line 2
-            {
-                List<IVertex> F = new List<IVertex>();
-                foreach (IVertex v in subGraph)
-                {
-                    if ( !v.Visited && v.Level == currentLevel ) //Line 3
-                    {
-                        F.Add(v);  // Line3
-                    }
-
-                    if ( F.Count == 0 ) // Line 4
-                    {
-                        //TODO: Check other threads if done
-                        //send(We are done?);
-                        return;  // Line 5
-                    }
-                }
-
-                //Line 7
-                List<IVertex> N = new List<IVertex>();
-                foreach (IVertex v in F)
-                {
-                    foreach (IEdge e in v.OutgoingEdges)
-                    {
-                        N.Add(e.Vertex2);
-                    }
-                }
-
-                //Lines 8 - 12
-                List<IVertex> newNeighbors = new List<IVertex>();
-                foreach (IVertex v in N)
-                {
-                    for ( int i = 0; i < NUM_THREADS; i++ )
-                    {
-                        List< List<IVertex> >Nq = new List< List <IVertex>>();
-                        if ( v.threadID == i )
-                        {
-                            Nq[i].Add(v);
-                        }
-                    }
-                    for (int i = 0; i < NUM_THREADS; i++)
-                    {
-                        if (i != myThreadID)
-                        {
-                            //TODO: Implement send
-                            //send(thread[i], Nq[i]);
-                        }
-                    }
-                    for (int i = 0; i < NUM_THREADS; i++)
-                    {
-                        List<IVertex> recieved;
-                        if (i != myThreadID)
-                        {
-                            //TODO: Implement recieve
-                            //recieved = recieve(thread[i]);
-                            //newNeighbors.AddRange(recieved);
-                        }
-                    }
-                }
-
-                N.AddRange(newNeighbors); // Line 13
-                //TODO: Check for repaeats and remove as needed (May not be neccicary)
-                foreach ( IVertex v in N)
-                {
-                    if ( v.Level == UInt32.MaxValue )
-                    {
-                        v.Level = currentLevel + 1;
-                    }
-                }
-            }
-        }
-
 
         static Smrf.NodeXL.Core.IVertex BreadthFirstSearch(IGraph graph, string searchKey, string searchValue)
         {
